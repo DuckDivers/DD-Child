7e8b19b2392e78fefc99b760c0f96df18e8dcfd6
Howard <howard@duckdiverllc.com>
2018-09-21 16:14:56 -0400

    Update functions.php

--- functions.php
+++ functions.php
@@ -4,7 +4,7 @@
 add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
 function my_theme_enqueue_styles() {
  
-    $parent_style = 'parent-style'; // This is 'twentyfifteen-style' for the Twenty Fifteen theme.
+    $parent_style = '_s-style'; // This is the name of the stylesheet for DuckDiverFramework
  
     wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.min.css' );
     wp_enqueue_style( 'child-style',
