c2b15759ac662701f81771c8fab6bec715416e1b
DuckDivers <howarde@scubaboard.com>
2018-10-17 15:39:43 -0400

    Comment Child Theme Options

--- inc/child-theme-options.php
+++ inc/child-theme-options.php
@@ -9,29 +9,29 @@
 $capability = 'edit_theme_options';
 
 // Option type
-$type = 'theme_mod'; // option / theme_mod
-
-$options[] = array( 'title'             => __( 'Theme Options', 'text-domain' ),
-                    'description'       => '',
-                    'id'                => 'child_theme_options',
-                    'priority'          => 10,
-                    'theme_supports'    => '',
-                    'type'              => 'panel' );
-// Section
-$options[] = array( 'title'             => __( 'Reserved', 'text-domain' ),
-                    'description'       => __( 'Section description', 'text-domain' ),
-                    'panel'             => 'child_theme_options',
-                    'id'                => 'slug_section_id',
-                    'priority'          => 10,
-                    'theme_supports'    => '',
-                    'type'              => 'section' );
-
-
-$options[] = array( 'title'             => __( 'Test Option', 'dd_theme' ),
-                    'description'       => __( 'Show the Search Box in the Navigation Bar', 'dd_theme' ),
-                    'section'           => 'slug_section_id',
-                    'id'                => 'test_new_option',
-					'default'			=> '1',
-                    'option'            => 'checkbox',
-                    'sanitize_callback' => '',
-                    'type'              => 'control' );
+//$type = 'theme_mod'; // option / theme_mod
+//
+//$options[] = array( 'title'             => __( 'Theme Options', 'text-domain' ),
+//                    'description'       => '',
+//                    'id'                => 'child_theme_options',
+//                    'priority'          => 10,
+//                    'theme_supports'    => '',
+//                    'type'              => 'panel' );
+//// Section
+//$options[] = array( 'title'             => __( 'Reserved', 'text-domain' ),
+//                    'description'       => __( 'Section description', 'text-domain' ),
+//                    'panel'             => 'child_theme_options',
+//                    'id'                => 'slug_section_id',
+//                    'priority'          => 10,
+//                    'theme_supports'    => '',
+//                    'type'              => 'section' );
+//
+//
+//$options[] = array( 'title'             => __( 'Test Option', 'dd_theme' ),
+//                    'description'       => __( 'Show the Search Box in the Navigation Bar', 'dd_theme' ),
+//                    'section'           => 'slug_section_id',
+//                    'id'                => 'test_new_option',
+//					'default'			=> '1',
+//                    'option'            => 'checkbox',
+//                    'sanitize_callback' => '',
+//                    'type'              => 'control' );
